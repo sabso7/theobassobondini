2cd5017346704035fa2a02b631de1cc3162d6861
Sabso <t.bassobondini@gmail.com>
2021-09-06 23:05:02 +0400

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sabso7/theobassobondini.git main:gh-pages
+git push -f git@github.com:sabso7/theobassobondini.git master:gh-pages
 
 cd -
